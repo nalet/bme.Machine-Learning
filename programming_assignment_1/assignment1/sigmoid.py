44920df2c69065a3bd83c30410b797b47aa95d57
Nalet Meinen <nalet.meinen@implentic.com>
2020-10-26 20:49:04 +0100

    clean assignment1

---
+++ programming_assignment_1/assignment1/sigmoid.py
@@ -0,0 +1,27 @@
+from numpy import exp
+
+
+def sigmoid(z):
+    """
+    Computes the sigmoid of z element-wise.
+
+    Args:
+        z: An np.array of arbitrary shape
+
+    Returns:
+        g: An np.array of the same shape as z
+
+    """
+
+    g = None
+    #######################################################################
+    # TODO:                                                               #
+    # Compute and return the sigmoid of z in g                            #
+    #######################################################################
+
+    pass
+
+    #######################################################################
+    #                         END OF YOUR CODE                            #
+    #######################################################################
+    return g
